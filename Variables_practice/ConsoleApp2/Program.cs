7251a9a5584faec0b0ba7742a5f49b814f915e62
Santiago Beltran <beltran.santiago1@gmail.com>
2018-10-23 20:32:51 -0700

    redfds

---
+++ Variables_practice/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Wat is your name? ");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoritenumber = Console.ReadLine();
+            //int favnum = Convert.ToInt32(favoritenumber);
+            //int total = favnum + 5;
+            //Console.WriteLine("Your favorite number is this " + total + " when you add 5");
+            //Console.ReadLine();
+
+            //bool cars = true;
+            //byte hoursworked = 42;
+            //sbyte negitive23 = -23;
+            //decimal money = 100.5m;
+            //double height = 211.12223;
+            //float secondsLeft = 2.62f;
+            //short tempunderTheSea = -70;
+
+            //int myAge = 22;
+            //string yearsOld = Convert.ToString(myAge);
+
+            //bool isRaining = true;
+            //string rainingStatuse = Convert.ToString(isRaining);
+            //Console.WriteLine("is it raining today? " + rainingStatuse);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ WhileLoopsDo/ConsoleApp7/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Try to guess my color!");
+
+            bool colorBool = false;
+
+            // With do statment
+            do
+            {
+
+                string userColor = Console.ReadLine();
+                string myColor = "red";
+
+
+                if (userColor == myColor)
+                {
+                    Console.WriteLine("That is correct! You picked my favorit color");
+                    userColor = Console.ReadLine();
+                    colorBool = true;
+
+                }
+
+
+                else
+                {
+                    Console.WriteLine("Thats  nice color but its not my color.");
+                    Console.WriteLine("Try to guess my color!");
+                }
+            }
+
+            while (colorBool == false);
+            {
+
+            }
+            // Just while loop
+            //while(colorBool == false)
+            //{
+
+            //    string userColor = Console.ReadLine();
+            //    string myColor = "red";
+
+
+            //    if (userColor == myColor)
+            //    {
+            //        Console.WriteLine("That is correct! You picked my favorit color");
+            //        userColor = Console.ReadLine();
+            //        colorBool = true;
+
+            //    }
+
+
+            //    else
+            //    {
+            //        Console.WriteLine("Thats  nice color but its not my color.");
+            //        Console.WriteLine("Try to guess my color!");
+            //    }
+
+
+            //}
+            Console.ReadLine();
+        }
+    }
+}
