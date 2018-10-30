d546a38b4b0887c9b2c451b2c85c0300475e1884
Santiago Beltran <beltran.santiago1@gmail.com>
2018-10-29 18:28:33 -0700

    more

---
+++ tryingToCatch/tryingToCatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<decimal> numbers = new List<decimal> { 1, 2, 3, 4, 5, 6};
+        Console.WriteLine("Please pick a number to divide by");
+
+        try
+        {
+            decimal userNumber = Convert.ToDecimal(Console.ReadLine());
+
+            foreach (decimal x in numbers)
+            {
+                decimal divided = x / userNumber;
+                Console.WriteLine(divided);
+            }
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.ReadLine();
+
+    }
+}
