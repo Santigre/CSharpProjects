98ba388dc1239ce161e6c141f23b629e954743f6
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-01 15:48:42 -0700

    more

---
+++ methodInt/methodInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intMath math = new intMath();
+            intMath math2 = new intMath();
+            intMath math3 = new intMath();
+
+            int firstNumber = 12;
+            decimal secondNumber = 44;
+            string thirdNumber = "1";
+
+            Console.WriteLine( math.firstMath(x: firstNumber));
+            Console.WriteLine( math.firstMath(x: secondNumber));
+            Console.WriteLine(math.firstMath(x: thirdNumber));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodInt/methodInt/intMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodInt
+{
+    class intMath
+    {
+        public int firstMath(int x)
+        {
+            int multiply = x * 3;
+            return multiply;
+        }
+        public decimal firstMath(decimal x)
+        {
+            decimal divide = x / 3;
+            int divideInt = Convert.ToInt32(divide);
+            return divide;
+        }
+        public int firstMath(string x)
+        {
+            int noodle = Convert.ToInt32(x);
+            int add = noodle + 32;
+            return add;
+
+        }
+    }
+}
