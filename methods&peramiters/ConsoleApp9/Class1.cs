2e8e580eb63cf7ecba25b1ff05da238976c86d1d
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-01 15:10:33 -0700

    more

---
+++ methods&peramiters/ConsoleApp9/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classOne
+
+{
+    public class Class1
+    {
+
+        public static void Numbers(int x, int y)
+        {
+
+
+            int numberOne = x + 31;
+
+            Console.WriteLine("Here is the second number {0}" , y);
+        }
+
+    }
+}
---
+++ methods&peramiters/ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using classOne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoInts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 firstNumber = new Class1();
+
+            Console.WriteLine("enter a number!");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a number!");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Class1.Numbers(x:numberOne, y:numberTwo);
+
+            Console.ReadLine(); 
+        }
+    }
+}
