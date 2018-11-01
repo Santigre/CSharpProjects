7212e735abb3a7606a8e2b88f9f765484b0c62ca
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-01 16:17:46 -0700

    more

---
+++ optionalMethodes/optionalMethodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalMethodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opting choiceOne = new opting();
+
+            Console.WriteLine("Please enter a number!! ");
+            int userFirst = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter an other number if you want. ");
+            string userSecond = Console.ReadLine();
+            int value;
+
+            if (int.TryParse(userSecond, out value) && value >= 1 && value <=3)
+            {
+                int secondNumber = Convert.ToInt32(userSecond);
+                Console.WriteLine(choiceOne.Choices(x: userFirst, y: secondNumber));
+            }
+            else
+            {
+                Console.WriteLine(choiceOne.Choices(x: userFirst));
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ optionalMethodes/optionalMethodes/opting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalMethodes
+{
+    class opting
+    {
+        public int Choices(int x, int y = 1)
+        {
+            int firstChoice = (x + 22) * y;
+            return firstChoice;
+        }
+    }
+}
