114a72f4fa43f50777388f26e2c6783dec283861
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-01 18:46:17 -0700

    more

---
+++ methodeOutputs/methodeOutputs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodeOutputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passing math = new Passing();
+            int output = math.dividing();
+            Console.WriteLine("Your number divided by 2 is: " + output);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodeOutputs/methodeOutputs/passing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodeOutputs
+{
+    class Passing 
+    {
+        
+        public static void number(out int x)
+        {
+            Console.WriteLine("Enter a number please!");
+            x = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        public int dividing(int y)
+        {
+            number(out y);
+            int output = y / 2;
+            return output;
+
+        }
+        public int dividing(string c)
+        {
+            int a = Convert.ToInt32(c);
+            number(out a);
+            int output = a / 2;
+            return output;
+
+        }
+
+        //public void text()
+        //{
+        //    Console.WriteLine("Your number divided by 2 is: ");
+        //    Console.WriteLine(dividing());
+        //}
+    }
+}
