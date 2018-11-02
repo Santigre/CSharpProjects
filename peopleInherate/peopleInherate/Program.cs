2be18a7bd24cda095e19466f82bc0cc68d65352f
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-01 19:42:32 -0700

    More

---
+++ peopleInherate/peopleInherate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peopleInherate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Individual = new Employee();
+            Individual.FirstName = "Sample";
+            Individual.LastName = "Student";
+            Individual.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
