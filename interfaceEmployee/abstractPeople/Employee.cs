a50aa2ab432a6357feeb3667195fb5a0c5e9d456
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-05 14:39:20 -0800

    more

---
+++ interfaceEmployee/abstractPeople/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractPeople
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit(Employee employee)
+        {
+        }
+    }
+}
---
+++ interfaceEmployee/abstractPeople/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            IQuittable quittable = new Employee();
+
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            employee.SayName();
+            
+            quittable.Quit(employee);
+
+            Console.ReadLine();
+        }
+    }
+}
