dfdf48bfd7e7fb3f72826f73d00d819b9e755ce7
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-05 17:48:40 -0800

    more

---
+++ GenaricEmployee/GenaricEmployee/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenaricEmployee
+{
+    public class Employee <T>
+    {
+        public List<T> Things = new List<T> {};
+
+    }
+}
---
+++ GenaricEmployee/GenaricEmployee/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenaricEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Things1 = new Employee<string>();
+            Employee<int> Things2 = new Employee<int>();
+
+            Things1.Things.Add("Hats");
+            Things1.Things.Add("Balls");
+            Things1.Things.Add("Cars");
+
+            Things2.Things.Add(1);
+            Things2.Things.Add(3);
+            Things2.Things.Add(87);
+
+            foreach(string thing in Things1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in Things2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
