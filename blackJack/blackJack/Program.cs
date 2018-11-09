42268e71b3c835bd898d5182b5837d47047a733c
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-08 18:50:29 -0800

    more

--- blackJack/blackJack/Program.cs
+++ blackJack/blackJack/Program.cs
@@ -10,69 +10,41 @@ namespace blackJack
     {
 
         static void Main(string[] args)
-        {
-
-
-            //card.Suit = Suit.Clubs;
-            //int underlyingValue = (int Suit
-
-            //blackJAck game = new blackJAck();
-            //game.Players = new List<string> { "Bob", "Bill", "Joe" };
-            //game.Listplayer();
-            //game.Play();
-            //Console.ReadLine();
-            //Game game = new blackJAck();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Bob";
-            //game += player;
-            //game -= player;
-
-
-
-
-
-            //blackJAck game = new blackJAck();
-            //game.Players = new List<string>() { "Bill", "Bob", "John" };
-            //game.Listplayer();
-            //Console.ReadLine();
-
-
+        {   
 
             Deck deck = new Deck();
-            deck.Shuffle(3);
 
-            //Player player = new Player();
+            //int count = deck.cards.count(x => x.face == face.ace);
+
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            //foreach( Card card in newList)
+            //{
+            //  Console.WriteLine(card.Face);
+            //}
+
+            //Console.WriteLine(count);
+
+            List<int> numberList = new List<int>() {1,2,3,4,5,66};
+
+            int sum = numberList.Where(x => x > 3).Sum();
+
+            //int min = numberList.Min();
+
+            //int min = numberList.Max();
+
+            Console.WriteLine(sum);
+
+
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
+
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-            //deck.Cards = new List<Card>();
-
-
-
-            //Card cardOne = new Card();
-            //cardOne.Face = "Queen";
-            //cardOne.Suit = "Spades";
-
-            //deck.Cards.Add(cardOne);
-
-            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
-
-        }
-
-
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for(int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
+            }
     }
 }
