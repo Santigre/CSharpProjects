21a14ad3e35ab73d99336b9d0c21d0a61148dfc0
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-08 18:27:29 -0800

    more

---
+++ structNumbers/structNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number i = new Number();
+
+            i.Amount = 12;
+
+            Console.WriteLine("My struct value is {0}", i.Amount);
+            Console.ReadLine();
+        }
+    }
+}
