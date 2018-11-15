f929a6d6eb405311bdbeb425addd2943790d9c65
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-14 18:49:49 -0800

    more

---
+++ loggingNumbers/loggingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace loggingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey you!!! Enter a number now!!!");
+            string userNumber = Console.ReadLine();
+            File.WriteAllText(@"C:\Repositories\CSharpProjects\loggingNumbers\numberLogs\loggedNumbers.txt", userNumber);
+            string readfile = File.ReadAllText(@"C:\Repositories\CSharpProjects\loggingNumbers\numberLogs\loggedNumbers.txt");
+            Console.WriteLine(readfile);
+            Console.ReadLine();
+        }
+    }
+}
