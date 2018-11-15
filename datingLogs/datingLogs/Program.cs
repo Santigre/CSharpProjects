152086f54ef76cc4bf02be3bd84a0ce0913f78be
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-14 19:16:24 -0800

    more

---
+++ datingLogs/datingLogs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datingLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime plusHours = DateTime.Now.AddHours(userNumber);
+            Console.WriteLine(plusHours);
+
+            Console.ReadLine();
+        }
+    }
+}
