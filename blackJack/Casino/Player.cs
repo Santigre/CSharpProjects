89b2679ab13ea457f70987f2ac2dd196cb9d10c0
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-16 17:59:02 -0800

    more

---
+++ blackJack/Casino/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Player
+    {
+        public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+        }
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivleyPLaying { get; set; }
+        public bool Stay { get; set; }
+
+
+        public bool Bet(int amount)
+        {
+            if(Balance - amount < 0)
+            {
+                Console.WriteLine("You dont have enough money!");
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+        public static Game operator +(Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+        public static Game operator -(Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+    }
+}
--- blackJack/blackJack/Program.cs
+++ blackJack/blackJack/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Casino;
+using Casino.blackJack;
+
 
 namespace blackJack
 {
@@ -10,20 +9,32 @@ namespace blackJack
     {
 
         static void Main(string[] args)
-        {   
-
-            Deck deck = new Deck();
+        {
 
 
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("Welcome player!! What is your name!?!?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money you got?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello {0}. Would you like to join a game of Black Jack right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if(answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new blackJAck();
+                game += player;
+                player.isActivleyPLaying = true;
+                while (player.isActivleyPLaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing");
             }
 
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Get out of here {0}! ", playerName);
             Console.ReadLine();
-            }
+
+        }
     }
 }
