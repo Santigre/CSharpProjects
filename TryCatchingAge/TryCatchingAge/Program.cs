91ebf3b914c135ed44b242230718b9990a1f0141
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-17 14:33:41 -0800

    Doing some trying and catching

---
+++ TryCatchingAge/TryCatchingAge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TryCatchingAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there! Please enter your age");
+
+            int userAge = int.Parse(Console.ReadLine());
+
+            bool validAge = false;
+
+
+            while (!validAge)
+            {
+                if (userAge <= 0)
+                {
+                    Console.WriteLine("Im sorry please enter a number that is over 0!");
+                    Console.ReadLine();
+                    return;
+                }
+                try
+                {
+
+                    DateTime thisYear = DateTime.Now;
+                    DateTime birthYear = thisYear.AddYears(-userAge);
+                    Console.WriteLine("You were born in the year: {0}", birthYear.Year);
+                    validAge = true;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Something went wrong. Try again!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
