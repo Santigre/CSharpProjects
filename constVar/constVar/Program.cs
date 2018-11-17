3f90e1fdf4f49698ec41a19aa28284e3a791ed5f
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-17 13:15:56 -0800

    adding const var again

---
+++ constVar/constVar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace constVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string welcome = "Hello World! ";
+            var check = "how are you doing?";
+
+            Console.WriteLine(welcome + check);
+        }
+    }
+}
