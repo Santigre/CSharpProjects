6be7ba80a52f31d5df54da6581937b9bb45b3cf3
Santiago Beltran <beltran.santiago1@gmail.com>
2018-11-27 22:06:59 -0800

    Car Insurance project added
    Got the logic but cant seem to get it to add into the db

---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using CarInsurance.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddQuote(DateTime dateOfBirth, int carYear, string carMake, string carModel, int speedingTickets, bool dui, bool fullCoverage)
+        {
+            var BaseQuote = 50;
+
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insuree = new Insuree();
+                insuree.DateOfBirth = dateOfBirth;
+                int Age = DateTime.Now.Year - dateOfBirth.Year;
+                if (Age < 25 || Age > 100)
+                {
+                    BaseQuote += 25;
+                }
+                if (Age < 18)
+                {
+                    BaseQuote += 100;
+                }
+
+                insuree.CarYear = carYear;
+                if (carYear < 2000 || carYear > 2015)
+                {
+                    BaseQuote += 25;
+                }
+
+                insuree.CarMake = carMake.ToLower();
+                insuree.CarModel = carModel.ToLower();
+                if (carMake == "porsche")
+                {
+                    BaseQuote += 25;
+                }
+
+                if (carMake == "porsche" && carModel == "911 carrera")
+                {
+                    BaseQuote += 25;
+                }
+
+                insuree.SpeedingTickets = speedingTickets;
+                if (speedingTickets > 0)
+                {
+                    BaseQuote = speedingTickets * 10;
+                }
+
+                insuree.DUI = dui;
+                if (dui == true)
+                {
+                    BaseQuote = BaseQuote + (BaseQuote / 4);
+                }
+
+                insuree.CoverageType = fullCoverage;
+                if (fullCoverage == true)
+                {
+                    BaseQuote = BaseQuote + (BaseQuote / 2);
+                }
+
+                insuree.Quote = BaseQuote;
+                db.SaveChanges();
+
+            }
+            return View();
+        }
+    }
+}
